6bbcefe0e6e443b8320ea22f2ce590be13e6f16f
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-01-25 12:11:34 -0200

    Removido o ButtonType e Implementado seu comportamento no InputType

--- src/SON/Elemento/Type/InputType.php
+++ src/SON/Elemento/Type/InputType.php
@@ -8,6 +8,7 @@ class InputType extends ElementoAbstract
 {
     private $type;
     private $placeholder;
+    private $value;
 
     public function __construct($id, $name,$class,$type)
     {
@@ -20,10 +21,19 @@ class InputType extends ElementoAbstract
         $this->placeholder = $placeholder;
     }
 
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
 
     public function render()
     {
-        // TODO: Implement render() method.
+        $output = "<input type='$this->type' id='$this->getid();' name='$this->getName();' class='$this->getClass();' ";
+        if(!is_null($this->value)){
+            $output += "value='$this->value'";
+        }
+        $output += "/>";
+        echo $output;
     }
 
 
